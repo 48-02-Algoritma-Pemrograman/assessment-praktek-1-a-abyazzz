9982caf94c237808c9857966ce4af6cf592b2446
abyazzz <abyazzz1045@gmail.com>
2024-10-24 11:37:37 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,18 @@
 public class Nomor1{
     public static void main(String[] args){
+    import java.util.Scanner;
+        Scanner scanner = new Scanner(System.in)
+         System.out.print("Total jam lembur  :  ");
+         int totalJamLembur = input.nextInt();
 
+          if (totalJamLembur <= 16){
+            honor = honor*totalJamLembur;
+            System.out.println(honor);
+        }
+        else if (totalJamLembur >= 16){
+            honor = honor*16+totalJamLembur-16;
+            System.out.println(honor);
+        }
+ 
     }
 }
