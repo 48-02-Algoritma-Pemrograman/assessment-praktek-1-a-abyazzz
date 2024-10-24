db75a1cb6ea3fd30f9d6eb6c3b4234eb08b96c73
abyazzz <abyazzz1045@gmail.com>
2024-10-24 12:28:15 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,19 +1,20 @@
 public class Nomor1{
     public static void main(String[] args){
     import java.util.Scanner;
-        int honor = 10000;
+         int honor = 10000;
         int Honor = 15000;
         int minLmb = 16;
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
          System.out.print("Total jam lembur  :  ");
-         int totalJamLembur = input.nextInt();
+         int totalJamLembur = scanner.nextInt();
 
-          if (totalJamLembur <= 16){
+         if (totalJamLembur <= 16){
             honor = honor*totalJamLembur;
             System.out.println(honor);
         }
         else if (totalJamLembur >= 16){
-            honor = honor*minLmb+totalJamLembur-minLmb;
+            honor = honor*minLmb;
+            honor = honor-(15000-10000);
             System.out.println(honor);
         }
  
